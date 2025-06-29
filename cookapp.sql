f657388c294248250e5c6899b26babe430e1ac2d
Catalina Olaechea <colaechea@frba.utn.edu.ar>
2025-06-29 12:42:46 -0300

    Base de datos

---
+++ cookapp.sql
@@ -0,0 +1,195 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 18-06-2025 a las 16:29:19
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `cookapp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `recipes`
+--
+
+CREATE TABLE `recipes` (
+  `recipe_id` int(11) NOT NULL,
+  `name` varchar(70) NOT NULL,
+  `description` text DEFAULT NULL,
+  `steps` varchar(250) NOT NULL,
+  `ingredients` varchar(250) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `create_at` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `roles`
+--
+
+INSERT INTO `roles` (`id`, `name`) VALUES
+(1, 'admin'),
+(2, 'user');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `email` varchar(250) NOT NULL,
+  `create_at` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `name`, `email`, `create_at`) VALUES
+(1, 'unusuario', 'estesoyyo', 'Catalina', 'colaechea@utn.frb.edu.ar', '2025-06-03 09:55:43'),
+(2, 'juan123', '$2b$10$AtJaeEiW2rkdxAiqZMg5geImoLKVHktSj6ZlRMGlakfSbhHfTmUpu', 'Juan Pérez', 'juan@example.com', '2025-06-08 10:31:44'),
+(3, 'colaechea', '$2b$10$NPVBOz5HKIAAdTH6403IU./HjgBvgddE3oRKhzxtaAo7sTlK1GpLS', 'catalina undefined', 'catalinaolaechea9@gmail.com', '2025-06-08 12:32:29'),
+(4, 'catalinamolaechea@gmail.com', '$2b$10$7u2qojN6XCKzQS.89wGKMutPJOT6DM1pa5LWvYHx3UUDTmB/npF7u', 'Emiliano Lopez', 'catalinamolaechea@gmail.com', '2025-06-11 01:42:34'),
+(5, 'colaechea@colegiosangabriel.edu.ar', '$2b$10$gjnwWbcZwMxcvirJZ4LAoek9Y7t2NAGS0KlFDUnUD9KyLuikpgPdq', 'Emiliano Lopez', 'colaechea@colegiosangabriel.edu.ar', '2025-06-11 18:46:37');
+
+--
+-- Disparadores `users`
+--
+DELIMITER $$
+CREATE TRIGGER `asignar_rol_usuario` AFTER INSERT ON `users` FOR EACH ROW BEGIN 
+  DECLARE userRoleId INT;
+  SELECT id INTO userRoleId FROM roles WHERE name = 'user' LIMIT 1;
+  IF userRoleId IS NOT NULL THEN
+    INSERT INTO user_roles (user_id, role_id) VALUES (NEW.id, userRoleId);
+  END IF;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user_roles`
+--
+
+CREATE TABLE `user_roles` (
+  `user_id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `user_roles`
+--
+
+INSERT INTO `user_roles` (`user_id`, `role_id`) VALUES
+(2, 2),
+(3, 2),
+(4, 2),
+(5, 2);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `recipes`
+--
+ALTER TABLE `recipes`
+  ADD PRIMARY KEY (`recipe_id`),
+  ADD KEY `usuario_id` (`user_id`);
+
+--
+-- Indices de la tabla `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `correo_electronico` (`email`),
+  ADD UNIQUE KEY `nombre_usuario` (`username`);
+
+--
+-- Indices de la tabla `user_roles`
+--
+ALTER TABLE `user_roles`
+  ADD PRIMARY KEY (`user_id`,`role_id`),
+  ADD KEY `role_id` (`role_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `recipes`
+--
+ALTER TABLE `recipes`
+  MODIFY `recipe_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `recipes`
+--
+ALTER TABLE `recipes`
+  ADD CONSTRAINT `recipes_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Filtros para la tabla `user_roles`
+--
+ALTER TABLE `user_roles`
+  ADD CONSTRAINT `user_roles_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `user_roles_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
